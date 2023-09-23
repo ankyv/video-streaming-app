5ae201e4ffdf613ad70508fc6164754e18e3a095
Ankit Varshney <thevarshneyankit@gmail.com>
2023-09-24 02:46:25 +0530

    Installed React Router

--- src/App.js
+++ src/App.js
@@ -4,15 +4,29 @@ import Header from "./components/Header";
 import Body from "./components/Body";
 import { Provider } from "react-redux";
 import store from "./utils/store";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 const App = () => {
   return (
     <Provider store={store}>
       <Header />
-      <Body />
+      <Outlet />
     </Provider>
   );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+    ],
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<RouterProvider router={appRouter} />);
