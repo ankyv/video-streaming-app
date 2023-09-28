542b80e1adab4af1877a63542a737b4bc63e8025
Ankit Varshney <thevarshneyankit@gmail.com>
2023-09-28 18:41:20 +0530

    Added infinite scroll to VideoSection component

--- src/components/VideoSection.js
+++ src/components/VideoSection.js
@@ -3,32 +3,49 @@ import { API_DATA_URL } from "../constants";
 import "dotenv/config";
 import VideoCard from "./VideoCard";
 import { Link } from "react-router-dom";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const VideoSection = () => {
-  const [videoList, setVideoList] = useState(null);
+  const [videoList, setVideoList] = useState([]);
+  const [pageToken, setPageToken] = useState("");
+
+  const API_URL =
+    API_DATA_URL +
+    process.env.API_KEY +
+    (pageToken && `&pageToken=${pageToken}`);
 
   useEffect(() => {
-    const API_URL = API_DATA_URL + process.env.API_KEY;
-    getVideoList(API_URL);
+    getVideoList();
   }, []);
 
-  async function getVideoList(API_URL) {
+  async function getVideoList() {
     const response = await fetch(API_URL);
     const json = await response.json();
-    setVideoList(json?.items);
+    setVideoList([...videoList, ...json?.items]);
+    json?.nextPageToken ? setPageToken(json?.nextPageToken) : setPageToken("");
   }
 
-  if (!videoList) return null;
+  if (!videoList.length) return null;
 
   return (
     <div className="video-section">
-      {videoList.map((video) => {
-        return (
-          <Link to={"/watch/" + video?.id} key={video?.id}>
-            <VideoCard video={video} />
-          </Link>
-        );
-      })}
+      <InfiniteScroll
+        dataLength={videoList.length}
+        next={() => {
+          getVideoList();
+        }}
+        hasMore={pageToken}
+        loader={<h4>Loading..</h4>}
+        className="video-section"
+      >
+        {videoList.map((video) => {
+          return (
+            <Link to={"/watch/" + video?.id} key={video?.id}>
+              <VideoCard video={video} />
+            </Link>
+          );
+        })}
+      </InfiniteScroll>
     </div>
   );
 };
